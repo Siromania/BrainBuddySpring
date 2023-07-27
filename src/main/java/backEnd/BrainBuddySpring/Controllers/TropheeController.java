b7c1bc38491daaaa6e58692941b82673f64d65d9
Adrien <Adrien>
2023-07-27 15:51:51 +0200

    Added PUT method to update a trophee and its games informations too

--- src/main/java/backEnd/BrainBuddySpring/Controllers/TropheeController.java
+++ src/main/java/backEnd/BrainBuddySpring/Controllers/TropheeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +18,14 @@ public class TropheeController {
 
 	@Autowired
 	private TropheeRepository tropheeRepository;
-
+	
+	// getAllTrophee
 	@GetMapping("/trophees")
 	public Iterable<Trophee> getTrophees() {
 		return this.tropheeRepository.findAll();
 	}
-
+	
+	// get a Trophee from an ID
 	@GetMapping("/trophees/{id}")
 	public Trophee getTrophee(@PathVariable Integer id) {
 		Optional<Trophee> optionalTrophee = this.tropheeRepository.findById(id);
@@ -32,9 +35,53 @@ public class TropheeController {
 		return optionalTrophee.get();
 	}
 
+	// create 
 	@PostMapping("/trophees")
-	public Trophee createUser(@RequestBody Trophee trophee) {
+	public Trophee createTrophee(@RequestBody Trophee trophee) {
 		return this.tropheeRepository.save(trophee);
+	}
+	
+	@PutMapping("/trophees/{id}")
+	public Trophee updateTrophee(@PathVariable("id") Integer id, @RequestBody Trophee trophee) {
+		Optional<Trophee> tropheeToUpdateOptional = this.tropheeRepository.findById(id);
+		if (!tropheeToUpdateOptional.isPresent()) {
+			return null;
+		}
+		
+		Trophee tropheeToUpdate = tropheeToUpdateOptional.get();
+		
+		// Update the name if it already exists
+		if (tropheeToUpdate.getName() !=null) {
+			tropheeToUpdate.setName(trophee.getName());
+		}
+		
+		if (tropheeToUpdate.getDescription() != null) {
+			tropheeToUpdate.setDescription(trophee.getDescription());			
+		}
+		
+		if (tropheeToUpdate.getImage() != null) {
+			tropheeToUpdate.setImage(trophee.getImage());			
+		}
+		
+		if (tropheeToUpdate.getImage() != null) {
+			tropheeToUpdate.setImage(trophee.getImage());			
+		}
+		// Update the game's informations of a Trophee
+		if (tropheeToUpdate.getGame().getGameId() != null) {
+			tropheeToUpdate.getGame().setGameId(trophee.getGame().getGameId());
+		}
+		if (tropheeToUpdate.getGame().getName() != null) {
+			tropheeToUpdate.getGame().setName(trophee.getGame().getName());
+		}
+		if (tropheeToUpdate.getGame().getDescription() != null) {
+			tropheeToUpdate.getGame().setDescription(trophee.getGame().getDescription());
+		}
+		if (tropheeToUpdate.getGame().getInstructions() != null) {
+			tropheeToUpdate.getGame().setInstructions(trophee.getGame().getInstructions());
+		}
+		
+		// Retourne un objet trophee actualisé grâce aux if précédents
+		return this.tropheeRepository.save(tropheeToUpdate);
 	}
 
 }
