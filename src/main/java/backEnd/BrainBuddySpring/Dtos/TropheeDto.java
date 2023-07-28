f925a2ac97b244738318d710be40d4e26efe4f23
Adrien <Adrien>
2023-07-28 14:49:23 +0200

    Trophee Controller Services Repository & DTO updated

---
+++ src/main/java/backEnd/BrainBuddySpring/Dtos/TropheeDto.java
@@ -0,0 +1,34 @@
+package backEnd.BrainBuddySpring.Dtos;
+
+public class TropheeDto {
+	private String name;
+	private String description;
+	private String image;
+	
+	public TropheeDto() {}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+}
---
+++ src/main/java/backEnd/BrainBuddySpring/Services/TropheeService.java
@@ -0,0 +1,29 @@
+package backEnd.BrainBuddySpring.Services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import backEnd.BrainBuddySpring.Entities.Trophee;
+import backEnd.BrainBuddySpring.Repositories.TropheeRepository;
+
+public class TropheeService {
+	@Autowired
+	private TropheeRepository tropheeRepository;
+
+	public Iterable<Trophee> findAllTrophees() {
+		return  this.tropheeRepository.findAll();
+	}
+
+	public Optional<Trophee> findTropheeById(Integer id) {
+		return tropheeRepository.findById(id);
+	}
+
+	public Trophee saveTrophee(Trophee trophee) {
+		return tropheeRepository.save(trophee);
+	}
+
+	public void deleteTrophee(Trophee trophee) {
+		this.tropheeRepository.delete(trophee);
+	}
+}
