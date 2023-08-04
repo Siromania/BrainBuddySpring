bbd439ae1bd97309b4e4cc1e73775b5aeda8b623
VAUBAN_C5\Administrateur <k.aberbach@hotmail.com>
2023-08-04 16:51:01 +0200

    cors

--- src/main/java/backEnd/BrainBuddySpring/Configurations/SecurityConfig.java
+++ src/main/java/backEnd/BrainBuddySpring/Configurations/SecurityConfig.java
@@ -8,13 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -54,6 +52,7 @@ public class SecurityConfig {
 	                		.requestMatchers("/token").permitAll()
 	                        .anyRequest().authenticated()
 	                        )
+	                .cors().and()
 	                .oauth2ResourceServer((oauth2)-> oauth2.jwt(Customizer.withDefaults()))
 	                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Spring Security will never create an HttpSession and it will never use it to obtain the Security Context
 //	                .httpBasic(Customizer.withDefaults()) // Spring Security’s HTTP Basic Authentication support is enabled by default, However, as soon as any servlet-based configuration is provided, HTTP Basic must be explicitly provided
